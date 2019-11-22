ebb38bded711146028393fa541a69eb403dbe004
nick honings <409398@student.fontys.nl>
2019-11-22 09:44:46 +0100

    d

---
+++ Roulette.DAL.MYSQL/UserContainerDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette.DAL.MYSQL
+{
+    public class UserContainerDAL
+    {
+        // Get all users
+        // -get full user profile
+        // -get bets
+        // -get battles
+
+        // Add user
+        // -add user profile
+        
+        // Delete user
+        // -delete user row
+
+        // Get User By Id
+        
+
+    }
+}
---
+++ Roulette.DAL.MYSQL/UserDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette.DAL.MYSQL
+{
+    public class UserDAL
+    {
+        // Update user
+        // -profile
+        // -currentbet
+        // -currentbattle
+
+    }
+}
