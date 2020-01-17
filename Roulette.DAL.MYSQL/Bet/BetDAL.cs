8a3ad7f5485e71ff86934c2df167f263fc03e2c6
nick honings <409398@student.fontys.nl>
2020-01-17 01:26:42 +0100

    Code Smell: Dead Code

--- Roulette.DAL.MYSQL/Bet/BetDAL.cs
+++ Roulette.DAL.MYSQL/Bet/BetDAL.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Roulette.DAL.MYSQL.Bet
 {
-    public class BetDAL: IBetDAL
+    public class BetDAL : IBetDAL
     {
         private readonly string _connection;
 
@@ -26,21 +26,20 @@ namespace Roulette.DAL.MYSQL.Bet
             {
                 sql += p + ",";
             }
-            sql = sql.Remove(sql.Length-1);
+            sql = sql.Remove(sql.Length - 1);
             sql += ") VALUES(";
             foreach (var p in propNames)
             {
-                sql += $"@{p},"; 
+                sql += $"@{p},";
             }
-            sql = sql.Remove(sql.Length-1);
+            sql = sql.Remove(sql.Length - 1);
             sql += ")";
 
             return sql;
         }
-        
+
         public bool Save(IBetDTO dto)
         {
-            //string sql = "INSERT INTO bet (Stake,Odd,Color,Number,Even,FirstNumber,SecondNumber) VALUES(@Stake,@Odd,@Color,@Number,@Even,@FirstNumber,@SecondNumber)";
             var values = dto.GetBetSpecificInfo();
             List<string> propNames = new List<string>();
             List<object> propValues = new List<object>();
@@ -53,7 +52,7 @@ namespace Roulette.DAL.MYSQL.Bet
                 }
             }
 
-            string sql = SQLStringBuilder(propNames);           
+            string sql = SQLStringBuilder(propNames);
 
 
             try
@@ -71,7 +70,7 @@ namespace Roulette.DAL.MYSQL.Bet
                             parameter.Value = propValues[i];
                             cmd.Parameters.Add(parameter);
                         }
-   
+
                         if (cmd.ExecuteNonQuery() > 0)
                         {
                             return true;
@@ -86,78 +85,4 @@ namespace Roulette.DAL.MYSQL.Bet
             }
         }
 
-        //private List<PropertyInfo> getUnderLyingProperties()
-        //{
-        //    List<PropertyInfo> properties = new List<PropertyInfo>();
-
-        //    if(typeof(T).Equals(typeof(IColorBetDTO)))
-        //    {
-        //        BetDTO dto = new BetDTO();
-        //        properties = dto.GetType().GetProperties().ToList();
-        //    }
-        //    return properties;
-        //}
-
-        private List<PropertyInfo> getUnderLyingProperties<T>()
-        {
-            List<PropertyInfo> properties = new List<PropertyInfo>();
-
-            //if (typeof(T).Equals(typeof(IColorBetDTO)))
-            //{
-            //    BetDTO dto = new BetDTO();
-            //    properties = dto.GetType().GetProperties().ToList();
-            //}
-            return properties;
-        }
-
-        private List<PropertyInfo> RemoveUnusedProps(string sql, List<PropertyInfo> properties)
-        {
-            for (int i = properties.Count; i >= 0; i--)
-            {
-                if(!sql.Contains(properties[i].Name))
-                {
-                    properties.RemoveAt(i);
-                }
-            }
-            return properties;
-        }
-
-        public bool Insert<T>(object[] param)
-        {
-            string sql = "INSERT INTO bet (Stake,Odd,Color,Number,Even,FirstNumber,SecondNumber) VALUES(@Stake,@Odd,@Color,@Number,@Even,@FirstNumber,@SecondNumber)";
-            
-            var properties = getUnderLyingProperties<T>();
-            properties = RemoveUnusedProps(sql, properties);
-
-            try
-            {
-                using (MySqlConnection conn = new MySqlConnection(_connection))
-                {
-                    conn.Open();
-                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
-                    {
-                        for (int i = 0; i < properties.Count; i++)
-                        {
-                            string propName = properties[i].Name;
-                            var parameter = cmd.CreateParameter();
-                            parameter.ParameterName = "@" + propName;
-                            parameter.Value = param[i];
-                            cmd.Parameters.Add(parameter);
-                        }
-                        if(cmd.ExecuteNonQuery() > 0)
-                        {
-                            return true;
-                        }
-                        return false;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message, ex);                
-            }
-        }
     }
-
-
-}
